42ad692b3bdb86c890291bcc1da1ef9069741c02
juliedundas <juliedundas@Julies-MacBook-Pro-2.local>
2020-03-17 21:46:11 -0700

    end of project

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -42,6 +42,10 @@ displayTable(tableData);
 
 // Getting a reference to the button on the page with the id property set to `filter-btn`
 var button = d3.select("#filter-btn");
+var button1 = d3.select("#filter-btn1");
+var button2 = d3.select("#filter-btn2");
+var button3 = d3.select("#filter-btn3");
+var button4 = d3.select("#filter-btn4");
 
 // Getting a reference to the input element on the page with the id property set to 'datetime'
 var inputField = d3.select("#datetime");
@@ -50,9 +54,11 @@ var inputField = d3.select("#datetime");
 //button.on("click", handleClick);
 
 // You can also define the click handler inline
-function handleChange() {
+function dateFilter() {
   //clear data on current table
   tbody.html("");
+  // stop the page from refresh
+  d3.event.preventDefault();
   //verify correct function is running
   console.log("Hi, the date filter button was clicked!");
   //create variable for value of input field
@@ -64,9 +70,127 @@ function handleChange() {
   );
   //display filtered table
   displayTable(dateFilter);
+  //displayTable(cityFilter);
+  //displayTable(stateFilter);
   //console.log(dateFilter);
 }
 
 // We can use the `on` function in d3 to attach an event to the handler function
-inputField.on("click", handleChange);
-button.on("click", handleChange);
+inputField.on("click", dateFilter);
+button.on("click", dateFilter);
+
+// Getting a reference to the input element on the page with the id property set to 'datetime'
+var inputCity = d3.select("#city");
+
+// You can also define the click handler inline
+function filterDate() {
+  //clear data on current table
+  tbody.html("");
+  // stop the page from refresh
+  d3.event.preventDefault();
+  //verify correct function is running
+  console.log("The city filter button was clicked!");
+  //create variable for value of input field
+  var inputCityElement = inputCity.property("value");
+
+  //filter by city
+  var cityFilter = tableData.filter(
+    ufoReport => ufoReport.city == inputCityElement
+  );
+  console.log(cityFilter);
+
+  //display filtered table
+  displayTable(cityFilter);
+  //console.log(dateFilter);
+}
+
+// We can use the `on` function in d3 to attach an event to the handler function
+inputCity.on("click", filterDate);
+button1.on("click", filterDate);
+
+// Getting a reference to the input element on the page with the id property set to 'datetime'
+var inputState = d3.select("#state");
+
+// You can also define the click handler inline
+function filterState() {
+  //clear data on current table
+  tbody.html("");
+  // stop the page from refresh
+  d3.event.preventDefault();
+  //verify correct function is running
+  console.log("The filter state button was clicked!");
+  //create variable for value of input field
+  var inputStateElement = inputState.property("value");
+
+  //filter by city
+  var stateFilter = tableData.filter(
+    ufoReport => ufoReport.state == inputStateElement
+  );
+  console.log(stateFilter);
+
+  //display filtered table
+  displayTable(stateFilter);
+  //console.log(dateFilter);
+}
+
+// We can use the `on` function in d3 to attach an event to the handler function
+inputCity.on("click", filterState);
+button2.on("click", filterState);
+
+// Getting a reference to the input element on the page with the id property set to 'datetime'
+var inputCountry = d3.select("#country");
+
+// You can also define the click handler inline
+function filterCountry() {
+  //clear data on current table
+  tbody.html("");
+  // stop the page from refresh
+  d3.event.preventDefault();
+  //verify correct function is running
+  console.log("The filter country button was clicked!");
+  //create variable for value of input field
+  var inputCountryElement = inputCountry.property("value");
+
+  //filter by city
+  var countryFilter = tableData.filter(
+    ufoReport => ufoReport.country == inputCountryElement
+  );
+  console.log(countryFilter);
+
+  //display filtered table
+  displayTable(countryFilter);
+  //console.log(dateFilter);
+}
+
+// We can use the `on` function in d3 to attach an event to the handler function
+inputCity.on("click", filterCountry);
+button3.on("click", filterCountry);
+
+// Getting a reference to the input element on the page with the id property set to 'datetime'
+var inputShape = d3.select("#shape");
+
+// You can also define the click handler inline
+function filterShape() {
+  //clear data on current table
+  tbody.html("");
+  // stop the page from refresh
+  d3.event.preventDefault();
+  //verify correct function is running
+  console.log("The filter shape button was clicked!");
+  //create variable for value of input field
+  var inputShapeElement = inputShape.property("value");
+
+  //filter by city
+  var shapeFilter = tableData.filter(
+    ufoReport => ufoReport.shape == inputShapeElement
+  );
+  console.log(shapeFilter);
+
+  //display filtered table
+  displayTable(shapeFilter);
+  //console.log(dateFilter);
+}
+
+// We can use the `on` function in d3 to attach an event to the handler function
+inputCity.on("click", filterShape);
+button4.on("click", filterShape);
